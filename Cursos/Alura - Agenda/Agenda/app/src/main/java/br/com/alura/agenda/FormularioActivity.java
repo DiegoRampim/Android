65de4109a53903bbe7ab86815d99e0a097dc4ffb
Diego Rampim - Casa <drampim@hotmail.com>
2017-02-25 20:33:59 -0300

    Projeto desenvolvido no curso de Android I da Alura. Manipulação de Activity. Banco de dados (CRUD).

---
+++ Cursos/Alura - Agenda/Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -0,0 +1,74 @@
+package br.com.alura.agenda;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.modelo.Aluno;
+
+public class FormularioActivity extends AppCompatActivity {
+
+
+    private FormularioHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        helper = new FormularioHelper(this);
+
+        Intent intent = getIntent();
+        Aluno aluno = (Aluno) intent.getSerializableExtra("aluno");
+
+        if(aluno != null){
+             helper.preencheFormulario(aluno);
+        }
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
+
+                Aluno aluno = helper.pegaAluno();
+                AlunoDAO dao = new AlunoDAO(this);
+                if(aluno.getId() > 0){
+                    dao.altera(aluno);
+                }else {
+                    dao.insere(aluno);
+                }
+
+                dao.close();
+                finish();
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
